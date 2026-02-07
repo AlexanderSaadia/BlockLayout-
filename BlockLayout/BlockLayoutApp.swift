243af4d8c9951003ba3d9c6663d13898a2b5fd06
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-02-07 10:57:33 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Alexander Saadia on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
